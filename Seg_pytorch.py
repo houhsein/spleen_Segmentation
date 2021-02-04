8a85904fdb8656c33d18158c6ea79974df769812
Aldoc <40588019+houhsein@users.noreply.github.com>
2021-02-04 10:48:26 +0800

    Create Seg_pytorch.py

---
+++ Seg_pytorch.py
@@ -0,0 +1,173 @@
+import glob
+import os
+import shutil
+import tempfile
+
+import IPython
+import matplotlib.pyplot as plt
+import torch
+import random
+
+from Unet impport UNet
+from monai.apps import download_and_extract
+from monai.config import print_config
+from monai.data import CacheDataset, DataLoader, Dataset
+from monai.inferers import sliding_window_inference
+from monai.losses import DiceLoss
+from monai.metrics import compute_meandice
+from monai.networks.layers import Norm
+from monai.networks.nets import UNet
+from monai.transforms import (
+    AsDiscrete,
+    AddChanneld,
+    Compose,
+    CropForegroundd,
+    LoadNiftid,
+    Orientationd,
+    RandCropByPosNegLabeld,
+    ScaleIntensityRanged,
+    Spacingd,
+    ToTensord,
+)
+from monai.utils import first, set_determinism
+import nibabel as nib
+from torchsummary import summary
+print_config()
+
+
+''' Dataloader '''
+data_dir = "/tf/jacky831006/Task09_Spleen/"
+train_images = sorted(glob.glob(os.path.join(data_dir, "imagesTr", "*.nii.gz")))
+train_labels = sorted(glob.glob(os.path.join(data_dir, "labelsTr", "*.nii.gz")))
+data_dicts = [
+    {"image": image_name, "label": label_name}
+    for image_name, label_name in zip(train_images, train_labels)
+]
+train_files, val_files = data_dicts[:-9], data_dicts[-9:]
+
+''' Data augmentation '''
+
+set_determinism(seed=0)
+train_transforms = Compose(
+    [
+        LoadNiftid(keys=["image", "label"]),
+        AddChanneld(keys=["image", "label"]),
+        Spacingd(keys=["image", "label"], pixdim=(1.5, 1.5, 2.0), mode=("bilinear", "nearest")),
+        Orientationd(keys=["image", "label"], axcodes="RAS"),
+        ScaleIntensityRanged(
+            keys=["image"], a_min=-57, a_max=164, b_min=0.0, b_max=1.0, clip=True,
+        ),
+        CropForegroundd(keys=["image", "label"], source_key="image"),
+        RandCropByPosNegLabeld(
+            keys=["image", "label"],
+            label_key="label",
+            spatial_size=(96, 96, 96),
+            pos=1,
+            neg=1,
+            num_samples=4,
+            image_key="image",
+            image_threshold=0,
+        ),
+        # user can also add other random transforms
+        # RandAffined(keys=['image', 'label'], mode=('bilinear', 'nearest'), prob=1.0, spatial_size=(96, 96, 96),
+        #             rotate_range=(0, 0, np.pi/15), scale_range=(0.1, 0.1, 0.1)),
+        ToTensord(keys=["image", "label"]),
+    ]
+)
+val_transforms = Compose(
+    [
+        LoadNiftid(keys=["image", "label"]),
+        AddChanneld(keys=["image", "label"]),
+        Spacingd(keys=["image", "label"], pixdim=(1.5, 1.5, 2.0), mode=("bilinear", "nearest")),
+        Orientationd(keys=["image", "label"], axcodes="RAS"),
+        ScaleIntensityRanged(
+            keys=["image"], a_min=-57, a_max=164, b_min=0.0, b_max=1.0, clip=True,
+        ),
+        CropForegroundd(keys=["image", "label"], source_key="image"),
+        ToTensord(keys=["image", "label"]),
+    ]
+)
+
+train_ds = CacheDataset(data=train_files, transform=train_transforms, cache_rate=1)
+# train_ds = monai.data.Dataset(data=train_files, transform=train_transforms)
+
+# use batch_size=2 to load images and use RandCropByPosNegLabeld
+# to generate 2 x 4 images for network training
+train_loader = DataLoader(train_ds, batch_size=2, shuffle=True)
+
+val_ds = CacheDataset(data=val_files, transform=val_transforms, cache_rate=1)
+# val_ds = Dataset(data=val_files, transform=val_transforms)
+val_loader = DataLoader(val_ds, batch_size=1)
+
+''' Train '''
+max_epochs = 600
+val_interval = 2
+best_metric = -1
+best_metric_epoch = -1
+epoch_loss_values = []
+metric_values = []
+post_pred = AsDiscrete(argmax=True, to_onehot=True, n_classes=2)
+post_label = AsDiscrete(to_onehot=True, n_classes=2)
+root_dir = "/tf/jacky831006/jupyter_data/densenet_3d/"
+
+for epoch in range(max_epochs):
+    print("-" * 10)
+    print(f"epoch {epoch + 1}/{max_epochs}")
+    model.train()
+    epoch_loss = 0
+    step = 0
+    for batch_data in train_loader:
+        step += 1
+        inputs, labels = (
+            batch_data["image"].to(device),
+            batch_data["label"].to(device),
+        )
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = loss_function(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        epoch_loss += loss.item()
+        print(
+            f"{step}/{len(train_ds) // train_loader.batch_size}, "
+            f"train_loss: {loss.item():.4f}")
+    epoch_loss /= step
+    epoch_loss_values.append(epoch_loss)
+    print(f"epoch {epoch + 1} average loss: {epoch_loss:.4f}")
+
+    if (epoch + 1) % val_interval == 0:
+        model.eval()
+        with torch.no_grad():
+            metric_sum = 0.0
+            metric_count = 0
+            for val_data in val_loader:
+                val_inputs, val_labels = (
+                    val_data["image"].to(device),
+                    val_data["label"].to(device),
+                )
+                roi_size = (160, 160, 160)
+                sw_batch_size = 4
+                val_outputs = sliding_window_inference(
+                    val_inputs, roi_size, sw_batch_size, model)
+                val_outputs = post_pred(val_outputs)
+                val_labels = post_label(val_labels)
+                value = compute_meandice(
+                    y_pred=val_outputs,
+                    y=val_labels,
+                    include_background=False,
+                )
+                metric_count += len(value)
+                metric_sum += value.sum().item()
+            metric = metric_sum / metric_count
+            metric_values.append(metric)
+            if metric > best_metric:
+                best_metric = metric
+                best_metric_epoch = epoch + 1
+                torch.save(model.state_dict(), os.path.join(
+                    root_dir, "Seg_best_metric.pth"))
+                print("saved new best metric model")
+            print(
+                f"current epoch: {epoch + 1} current mean dice: {metric:.4f}"
+                f"\nbest mean dice: {best_metric:.4f} "
+                f"at epoch: {best_metric_epoch}"
+            )
